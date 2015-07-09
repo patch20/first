747218651ba25a69b71c37a21db99ce88b9eaa5d
Nizamodeen Abrahim <patch.18.24@gmail.com>
2015-07-09 18:46:08 -0400

    update 1

--- ProjectWithGit/src/hi.java
+++ ProjectWithGit/src/hi.java
@@ -3,7 +3,7 @@ public class hi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Fun with Git");
+		System.out.println("Fun with Git billion");
 	}
 
 }
